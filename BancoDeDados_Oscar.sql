b94fe6d93b3878ec7d0b17679e65b89c15eb84f4
Gustavo Portela <ggomesportela26@gmail.com>
2024-10-28 16:16:07 -0300

    add csv file

--- BancoDeDados_Oscar.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO indicados_ao_oscar (ano_filmagem, ano_cerimonia, cerimonia, categoria, nome_do_indicado, nome_do_filme, vencedor)
-VALUES (2024, 2025, 97, 'ACTRESS IN A LEADING ROLE', 'Demi Moore', 'The Substance', 1), 
-(2024, 2025, 97, 'ACTRESS IN A LEADING ROLE', 'Blake Lively', 'É assim que acaba', 0),
-(2023, 2024, 96, 'ACTRESS IN A LEADING ROLE', 'Kiernan Shipka', 'Dezesseis Facadas', 1);
